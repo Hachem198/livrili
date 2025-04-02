25871a83e04483373f2a241ae83f6f61285c86b6
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2025-04-02 07:06:24 +0100

    Main: adding email and phoneNumber so the user can see them and adding modify userinfo api

--- backend/src/main/java/com/sfar/livrili/Controller/AuthController.java
+++ backend/src/main/java/com/sfar/livrili/Controller/AuthController.java
@@ -2,6 +2,7 @@ package com.sfar.livrili.Controller;
 
 import com.sfar.livrili.Domains.Dto.AuthDto.AuthResponseDto;
 import com.sfar.livrili.Domains.Dto.AuthDto.LoginRequestDto;
+import com.sfar.livrili.Domains.Dto.AuthDto.ModifyUserRequestDto;
 import com.sfar.livrili.Domains.Dto.AuthDto.UserDtoRequest;
 import com.sfar.livrili.Domains.Dto.ErrorDto.ApiErrorResponse;
 import com.sfar.livrili.Domains.Entities.User;
@@ -24,6 +25,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("v1/api/auth")
@@ -76,6 +81,17 @@ public class AuthController {
         public ResponseEntity<Object> getUser(HttpServletRequest request) {
                 UUID userId = (UUID) request.getAttribute("userId");
                 User user = userService.getUser(userId);
+                return new ResponseEntity<>(userMapper.ToClientOrDeliveryPerson(user), HttpStatus.OK);
+        }
+        @Operation(summary = "Update User Info", description = "Updates the authenticated user's details.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "User updated", content = @Content(schema = @Schema(implementation = User.class))),
+                        @ApiResponse(responseCode = "401", description = "Invalid Token", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+        })
+        @PutMapping()
+        public ResponseEntity<Object> modifyUser(@RequestBody ModifyUserRequestDto modifyUserRequest, HttpServletRequest request) {
+                UUID userId = (UUID) request.getAttribute("userId");
+                User user = userService.modifyUser(modifyUserRequest, userId);
                 return new ResponseEntity<>(userMapper.ToClientOrDeliveryPerson(user), HttpStatus.OK);
         }
 }
